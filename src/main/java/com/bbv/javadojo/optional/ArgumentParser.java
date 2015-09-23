fa3557d5451b9e85b705d147b656d3134419226b
Ilkka Harmanen <ilkka.harmanen@sharoo.com>
2015-09-23 18:21:48 +0200

    fixed constructor for args array with null element

--- src/main/java/com/bbv/javadojo/optional/ArgumentParser.java
+++ src/main/java/com/bbv/javadojo/optional/ArgumentParser.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import static java.lang.String.valueOf;
+import static java.util.Arrays.asList;
 
 public class ArgumentParser {
 
@@ -13,14 +14,14 @@ public class ArgumentParser {
     private final String[] args;
 
     public ArgumentParser(final String schema, final String[] args) {
-        if (schema == null || args == null)
+        if (schema == null || args == null || asList(args).contains(null))
             throw new ArgsException("constructor parameters may not be null");
         this.schema = schema;
         this.args = args;
     }
 
     public boolean getBoolean(final char arg) {
-        return Arrays.asList(args).contains(valueOf("-" + arg));
+        return asList(args).contains(valueOf("-" + arg));
     }
 
     public String getString(final char arg) {
