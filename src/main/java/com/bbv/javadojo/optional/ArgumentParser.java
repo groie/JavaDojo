785bb1760b44dd5fed86e084383ce66b6955ecb2
Ilkka Harmanen <ilkka.harmanen@sharoo.com>
2015-09-23 21:10:53 +0200

    Found use for the nullable! implemented getInt

--- src/main/java/com/bbv/javadojo/optional/ArgumentParser.java
+++ src/main/java/com/bbv/javadojo/optional/ArgumentParser.java
@@ -2,7 +2,9 @@ package com.bbv.javadojo.optional;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
 
 public class ArgumentParser {
@@ -18,18 +20,20 @@ public class ArgumentParser {
     }
 
     public boolean getBoolean(final char arg) {
-        return args.contains(keyOf(arg));
+        return keyExists(arg);
     }
 
     //  - char*   - String arg.
     public String getString(final char arg) {
-        // TODO: we could still have multiple matches
-        int idx = args.indexOf(keyOf(arg));
-        return idx == -1 ? null : args.get(idx +1);
+        return getValueForKey(arg).orElse(null);
     }
 
     public int getInt(final char arg) {
-        return 0;
+        try {
+            return getValueForKey(arg).map(Integer::valueOf).orElse(null);
+        } catch (NumberFormatException e) {
+            throw new ArgsException(format("%s could not be parsed as an integer", getString(arg)), e);
+        }
     }
 
     public double getDouble(final char arg) {
@@ -47,6 +51,15 @@ public class ArgumentParser {
     public int nextArgument() {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    private boolean keyExists(char key) {
+        return args.contains(keyOf(key));
+    }
+
+    private Optional<String> getValueForKey(char key) {
+        int idx = args.indexOf(keyOf(key));
+        return idx == -1 ? Optional.empty() : Optional.ofNullable(args.get(idx+1));
     }
 
     private static String keyOf(final char arg) {
