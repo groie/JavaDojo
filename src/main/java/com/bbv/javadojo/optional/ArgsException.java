134c43c4bb2a8a84e3e532dacf74fc029a7174f9
Ilkka Harmanen <ilkka.harmanen@sharoo.com>
2015-09-23 17:26:46 +0200

    added args exception that was present in the code

--- src/main/java/com/bbv/javadojo/optional/ArgsException.java
+++ src/main/java/com/bbv/javadojo/optional/ArgsException.java
@@ -3,5 +3,8 @@ package com.bbv.javadojo.optional;
 /**
  * Created by gro on 23/09/15.
  */
-public class ArgsException extends Exception {
+public class ArgsException extends RuntimeException {
+    public Object errorMessage() {
+        return super.getMessage();
+    }
 }
---
+++ src/test/java/com/bbv/javadojo/optional/ArgumentParserSpec.groovy
@@ -0,0 +1,36 @@
+package com.bbv.javadojo.optional
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Created by gro on 23/09/15.
+ */
+class ArgumentParserSpec extends Specification {
+
+    @Unroll("Constructor with #schema and #args fails")
+    def "failed test constructor"() {
+        when:
+        new ArgumentParser(schema, args);
+
+        then:
+        thrown(ArgsException)
+
+        where:
+        schema << [null]
+        args << [null]
+    }
+
+    @Unroll("Constructor with #schema and #args succeeds")
+    def "successful test constructor"() {
+        when:
+        def parser = new ArgumentParser(schema, args);
+
+        then:
+        parser != null
+
+        where:
+        schema << ["f,s*,n#,a##,p[*]"]
+        args << ["-f -s Bob -n 1 -a 3.2 -p e1 -p e2 -p e3"]
+    }
+}
