751c1cecc78ad38b693c7d22753aa1508bef5436
Ilkka Harmanen <ilkka.harmanen@sharoo.com>
2015-09-23 17:21:57 +0200

    removed junit test class

--- src/test/java/com/bbv/javadojo/optional/ArgumentParserTest.java
+++
@@ -1,9 +0,0 @@
-package com.bbv.javadojo.optional;
-
-import org.junit.Test;
-public class ArgumentParserTest {
-    @Test
-    public void testCreateWithNoSchemaOrArguments() throws Exception {
-        final ArgumentParser args = new ArgumentParser("", new String[0]);
-    }
-}
